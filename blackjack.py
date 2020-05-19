39f46aca28e5875400a9a43e6ba4cd62cb5d9dd7
Stuart Bond <stuart.w.bond@gmail.com>
2020-05-19 13:03:02 +0100

    added split functionality

--- blackjack.py
+++ blackjack.py
@@ -6,13 +6,16 @@ deck = ["2 of hearts","3 of hearts","4 of hearts","5 of hearts","6 of hearts","7
 #define a counting function, takes input of a hand consisting of the above "cards" and returns the blackjack count
 def count_func(a_hand):
     count=0
-    for j in range(len(a_hand)):
+    for j in range(len(a_hand)):        
         if a_hand[j][:2] == "10" or a_hand[j][:2] == "Ja" or a_hand[j][:2] == "Qu" or a_hand[j][:2] == "Ki":
             count=count+10
         elif a_hand[j][:2] == "Ac":
             count=count+11
         else:
             count=count+int(a_hand[j][:1])
+        if count>21:
+            if a_hand[0][:2] == "Ac" or a_hand[1][:2] == "Ac":
+                count=count-10
     return count
 
 import numpy
@@ -57,6 +60,11 @@ hands=[]
 for m in range(number_of_players+1):
     hands.append([])
 
+#generate empty list to hold split hands
+hands_split=[]
+for m in range(number_of_players+1):
+    hands_split.append([])
+
 #give everyone 2 cards, give dealer one card (dealers hand is held in last element of list)
 for i in range(number_of_players):
     for j in range(2):
@@ -68,11 +76,6 @@ for i in range(number_of_players,number_of_players+1):
     hands[i].append(deck[x])
     deck.remove(deck[x]) 
 
-
-
-
-
-
 #make list of states for each player
 #state 0 means in play, can hit or stand
 #state 1 means has manually stood
@@ -80,8 +83,13 @@ for i in range(number_of_players,number_of_players+1):
 states=[]
 for m in range(number_of_players):
     states.append(int(0))
+#generate states of split hands
+states_split=[]
+for m in range(number_of_players):
+    states_split.append(int(0))
 
 #the main game loop
+#checks if can split, offers if can
 #should run through each player, asking hit or stand
 #if hit, checks if bust
 #if all stand or bust, dealer draws until winner is found
@@ -99,7 +107,60 @@ for i in range(number_of_players):
     time.sleep(1)
     print("Player %i has %s." % (int(i+1), hands[i]))
     time.sleep(1)
-    while True:   
+
+    #check if hand is splittable, offer split, 
+    # put second card into new hand and deal both hands to 2 cards
+    if hands[i][0][:2] == hands[i][1][:2]:
+        print("")
+        print("Player %i can split." % int(i+1))
+        print("")
+        time.sleep(1)
+        print("Would you like to split? Yes/No:")
+        print("")
+        split_pair = str(input())
+        acceptable_split_inputs = ["Yes","yes","y","Y","No","no","n","N"]
+        while True:
+            try:
+                #if yes, split the hand
+                #if no, break and continue to main loop
+                #else invalid to proceed to exception
+                if split_pair in acceptable_split_inputs:
+                    if split_pair == "Yes" or split_pair == "yes" or split_pair == "y" or split_pair == "Y":
+                        time.sleep(1)
+                        print("")    
+                        print("Player %i splits their pair and is given extra cards." % int(i+1))
+                        print("")
+                        hands_split[i].append(hands[i][1])
+                        hands[i].remove(hands[i][1])
+                        x = randint(0,len(deck)-1)
+                        hands[i].append(deck[x])
+                        deck.remove(deck[x]) 
+                        x = randint(0,len(deck)-1)
+                        hands_split[i].append(deck[x])
+                        deck.remove(deck[x]) 
+                        break
+                    else:
+                        print("")
+                        print("Player %i does not split their hand." % int(i+1))
+                        print("")
+                        break
+                else:
+                    x=2/0
+            except:
+                #exception to guaruntee appropriate input
+                time.sleep(0.5)
+                print("")
+                print("Must enter 'yes' or 'no'!!")
+                print("")
+                time.sleep(1)
+                print("Would you like to split!? Yes/No:")
+                print("")
+                split_pair = str(input())
+
+
+
+    #the main loop
+    while True:
         print("")    
         print("Player %i: Hit or stand?" % int(i+1))
         print("")
@@ -155,8 +216,8 @@ print("The dealer has %s." % hands[number_of_players])
 while count_func(hands[number_of_players]) < 17:
     x = randint(0,len(deck)-1)
     hands[number_of_players].append(deck[x])
-    print("")
     time.sleep(1)
+    print("")
     print("Dealer draws %s." % deck[x])
     print("")
     deck.remove(deck[x])
@@ -208,3 +269,8 @@ if dealer_count < 22:
                 print("")
 
 
+
+#add split functionality
+
+#note splitting half implemented, not functional properly
+#breaks loop early skipping main loop for non-split hands
